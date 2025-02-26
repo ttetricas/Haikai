1156d27588620bdefa7fb034d19f8fbc64c12e64
ttetricas <estermeireles89@gmail.com>
2025-02-26 14:33:20 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Haikai");
+Console.WriteLine("\n");
+Console.WriteLine("vazio agudo");
+Console.WriteLine("ando meio");
+Console.WriteLine("cheio de tudo");
+Console.WriteLine("\n");
+Console.WriteLine("Paulo Leminski");
